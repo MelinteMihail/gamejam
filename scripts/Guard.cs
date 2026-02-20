17d491e34be07e10f13311a8e7093572d5109b5d
MelinteMihail <mihailmelinte74@gmail.com>
2026-02-20 14:18:11 +0200

    add guard, fix dialogue

---
+++ scripts/Guard.cs
@@ -0,0 +1,92 @@
+using Godot;
+
+public partial class Guard : Node2D
+{
+    [Export] 
+    private Label interactLabel;
+
+    private GuardState guardState;
+    private Dialogue dialogue;
+
+    private string NextScenePath = "res://scenes/game.tscn";
+    private bool shouldTransitionAfterDialogue = false;
+    private bool playerNearby = false;
+
+    private (string speaker, string text)[] firstTimeDialogue = new[]
+    {
+        ("Guard", "Halt! Who goes there?"),
+        ("Guard", "I've never seen you around here before."),
+        ("Guard", "Pass through, but stay out of trouble.")
+    };
+
+    private (string speaker, string text)[] repeatDialogue = new[]
+    {
+        ("Guard", "Move along."),
+    };
+
+    public override void _Ready()
+    {
+        SetProcessInput(true);
+
+        guardState = GetNode<GuardState>("/root/GuardState");
+        dialogue = GetNode<Dialogue>("/root/game/UI/Dialog");
+
+        var area = GetNode<Area2D>("Interactable Area");
+        area.BodyEntered += OnBodyEntered;
+        area.BodyExited += OnBodyExited;
+
+        dialogue.DialogueClosed += OnDialogueClosed;
+
+        interactLabel.Text = "Press [E] to interact";
+        interactLabel.Hide();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (Input.IsActionJustPressed("interact"))
+            GD.Print($"Interact pressed. playerNearby={playerNearby}, dialogueVisible={dialogue.Visible}");
+
+        if (playerNearby && Input.IsActionJustPressed("interact") && !dialogue.Visible)
+        {
+            GD.Print("Opening dialogue...");
+            if (!guardState.HasSpokenToGuard)
+            {
+                dialogue.ShowDialogue(firstTimeDialogue);
+                guardState.HasSpokenToGuard = true;
+                shouldTransitionAfterDialogue = true;
+            }
+            else
+            {
+                dialogue.ShowDialogue(repeatDialogue);
+                shouldTransitionAfterDialogue = true;
+            }
+        }
+    }
+
+    private void OnBodyEntered(Node2D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            playerNearby = true;
+            interactLabel.Show();
+        }
+    }
+
+    private void OnBodyExited(Node2D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            playerNearby = false;
+            interactLabel.Hide();
+        }
+    }
+
+    private void OnDialogueClosed()
+    {
+        if (shouldTransitionAfterDialogue)
+        {
+            LoadingScreen.NextScenePath = NextScenePath;
+            GetTree().ChangeSceneToFile("res://scenes/loading_screen.tscn");
+        }
+    }
+}
---
+++ scripts/GuardState.cs
@@ -0,0 +1,6 @@
+using Godot;
+
+public partial class GuardState : Node
+{
+    public bool HasSpokenToGuard = false;
+}
